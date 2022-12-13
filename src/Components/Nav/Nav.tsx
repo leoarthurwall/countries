f49595266d67f292f46ea86ff3be7ea7f26046fd
leoarthurwall <leowall93@gmail.com>
2022-12-13 21:46:03 +0000

    added dark mode container and text. about to import moon icon

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,28 +1,42 @@
 import styled from "styled-components";
 
 const Container = styled.nav`
-    height: 60px;
-    widht: 100%;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-    padding:  20px 40px;
-    border: 1px solid blue;
-    box-sizing: border-box;
-
+  height: 60px;
+  widht: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  border: 1px solid blue;
+  box-sizing: border-box;
 `;
 const Header = styled.h1`
   margin: 0;
   padding: 0;
   font-size: 20px;
   font-weight: 600;
-  border: 1px solid red;
   width: fit-content;
+`;
+
+const DarkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding
+`;
+const DarkText = styled.h4`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 600;
 `;
 const Nav = () => {
   return (
     <Container>
       <Header>Where in the world?</Header>
+      <DarkContainer>
+        <DarkText>Dark Mode</DarkText>
+      </DarkContainer>
     </Container>
   );
 };
