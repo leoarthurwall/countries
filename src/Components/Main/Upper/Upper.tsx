2f552a3f3081044c6543c7e33f1271967e514054
leoarthurwall <leowall93@gmail.com>
2022-12-13 22:04:21 +0000

    created an upper component where the search and dropdown will go

---
+++ src/Components/Main/Upper/Upper.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  width: 100%;
+  padding: 40px 70px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid blue;
+`;
+const Upper = () => {
+  return <Container></Container>;
+};
+
+export default Upper;
