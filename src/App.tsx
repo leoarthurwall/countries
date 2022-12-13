a2e3dcd5508d6bae2fe010619d5595750d735869
leoarthurwall <leowall93@gmail.com>
2022-12-13 13:51:58 +0000

    set up main and nav folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.css';
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const Header = styled.header``
+const Container = styled.main`
+height: 100vh;
+width: 100vw;
+
+display: flex:
+flex-direction: column;
+`;
+const Header = styled.header``;
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-        Hello World
-      </Header>
-    </div>
+    <Container>
+      <Header>Hello World</Header>
+    </Container>
   );
 }
 
