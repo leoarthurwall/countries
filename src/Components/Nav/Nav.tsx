45468594cddafbea4aa6e0263c7c31f707bcbed6
leoarthurwall <leowall93@gmail.com>
2022-12-13 21:41:57 +0000

    added the header and laid it out within the Nav container

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import styled from "styled-components";
 
+const Container = styled.nav`
+    height: 60px;
+    widht: 100%;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    padding:  20px 40px;
+    border: 1px solid blue;
+    box-sizing: border-box;
+
+`;
+const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  font-weight: 600;
+  border: 1px solid red;
+  width: fit-content;
+`;
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <Container>
+      <Header>Where in the world?</Header>
+    </Container>
+  );
+};
 
-export default Nav
+export default Nav;
