cdf01f25f79580e2828c9a94ff4846f7e559ddda
leoarthurwall <leowall93@gmail.com>
2022-12-14 13:36:50 +0000

    set up a use effect that retches the country data. fetch is successful as consol log shows array of 250 objects

--- src/Components/Main/Card/Card.tsx
+++ src/Components/Main/Card/Card.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import CardSingle from "./CardSingle/CardSingle";
+import { useState, useEffect } from "react";
 
 const Container = styled.section`
   width: 100%;
@@ -13,12 +14,19 @@ const Container = styled.section`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-row-gap: 50px;
   grid-column-gap: 25px;
-
 `;
 
-
-
 const Card = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/all`)
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+            setData(json);
+          });
+      }, []);
   return (
     <Container>
       <CardSingle />
