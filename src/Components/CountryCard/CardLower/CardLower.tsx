471d31c0150bf0fe5fefcb8af53d08f6836cab85
leoarthurwall <leowall93@gmail.com>
2022-12-20 23:41:06 +0000

    created an a tag button that links to the map of the selected country

--- src/Components/CountryCard/CardLower/CardLower.tsx
+++ src/Components/CountryCard/CardLower/CardLower.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useCountry } from "../../../Context/CountryContext";
+import { BiMap } from "react-icons/bi";
 
 const Container = styled.div`
   display: flex;
@@ -14,13 +15,14 @@ const Container = styled.div`
 `;
 const Left = styled.section`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: start;
+  align-items: left;
   flex-direction: column;
   height: 100%;
   width: 50%;
   padding-right: 40px;
   box-sizing: border-box;
+  gap: 10px;
 `;
 
 const Right = styled.section`
@@ -42,13 +44,43 @@ const Name = styled.h3`
   margin: 0;
 `;
 const Info = styled.h4`
-margin: 0;
-`
+  margin: 0;
+`;
 const InfoSpan = styled.span`
-color: grey;
-font-weight: 300;
+  color: grey;
+  font-weight: 300;
+  font-size: 14px;
+`;
+
+
+const ViewMap = styled.a`
+display: flex;
+align-items: center;
+justify-content: center;
+height: auto;
+width: fit-content;
+padding: 12px 15px;
 font-size: 14px;
-`
+color: white;
+gap 10px;
+box-sizing: border-box;
+background-color: rgba(0, 0, 0, 0.4);
+border-radius: 25px;
+box-shadow: rgba(0, 0, 0, 0.16) 1px 3px 5px;
+text-decoration: none;
+cursor: pointer;
+transition: 0.2s;
+&:hover{
+    background-color: rgba(0, 0, 0, 0.5);
+}
+`;
+
+const MapText = styled.p`
+  color: white;
+  font-size: 14px;
+  font-weight: 500;
+  margin: 0;
+`;
 
 const CardLower = () => {
   const { countries } = useCountry();
@@ -57,17 +89,34 @@ const CardLower = () => {
     <Container>
       <Left>
         <Flag></Flag>
+        <ViewMap href="https://goo.gl/maps/Z9DXNxhf2o93kvyc6" target="_blank">
+          <MapText>View On Map</MapText>
+          <BiMap size={18}/>
+        </ViewMap>
       </Left>
       <Right>
         <Name>Germany</Name>
-        <Info>Population: <InfoSpan>5,000,000</InfoSpan> </Info>
-        <Info>Region: <InfoSpan>Europe</InfoSpan> </Info>
-        <Info>Capital: <InfoSpan>Berlin</InfoSpan> </Info>
-        <Info>Currency: <InfoSpan>Euro</InfoSpan> </Info>
-        <Info>Languages:<InfoSpan> German</InfoSpan> </Info>
-        <Info>Continent:<InfoSpan>South America</InfoSpan> </Info>
-        <Info>Area: <InfoSpan>2,000,000 km2</InfoSpan> </Info>
-        
+        <Info>
+          Population: <InfoSpan>5,000,000</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Region: <InfoSpan>Europe</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Capital: <InfoSpan>Berlin</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Currency: <InfoSpan>Euro</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Languages:<InfoSpan> German</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Continent:<InfoSpan>South America</InfoSpan>{" "}
+        </Info>
+        <Info>
+          Area: <InfoSpan>2,000,000 km2</InfoSpan>{" "}
+        </Info>
       </Right>
     </Container>
   );
