df7c064f220fe0936e83cbc9b566fe9f04f3bded
leoarthurwall <leowall93@gmail.com>
2022-12-20 21:06:19 +0000

    added close circle to carduppder container

--- src/Components/CountryCard/CardUpper/CardUpper.tsx
+++ src/Components/CountryCard/CardUpper/CardUpper.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
   justify-content: end;
   align-items: center;
-  height: 70px;
+  height: auto;
   width: 100%;
+`;
+
+const CloseCircle = styled.div`
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  transition: 0.2s;
+  margin: 20px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+  cursor: pointer;
 `;
 const CardUpper = () => {
   return (
-    <Container>CardUpper</Container>
-  )
-}
+    <Container>
+      <CloseCircle></CloseCircle>
+    </Container>
+  );
+};
 
-export default CardUpper
+export default CardUpper;
