12a177f9e1e8ff63f0d8cf94a21b11b26a29d7e2
leoarthurwall <leowall93@gmail.com>
2022-12-20 23:22:14 +0000

    added placeholder info on Card Right

--- src/Components/CountryCard/CardLower/CardLower.tsx
+++ src/Components/CountryCard/CardLower/CardLower.tsx
@@ -8,35 +8,47 @@ const Container = styled.div`
   align-items: center;
   height: 100%;
   width: 100%;
-  padding: 40px;
+  padding: 80px 40px;
   border: 2px solid red;
   box-sizing: border-box;
 `;
 const Left = styled.section`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   flex-direction: column;
   height: 100%;
   width: 50%;
-  padding-right: 20px;
+  padding-right: 40px;
   box-sizing: border-box;
 `;
 
 const Right = styled.section`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: start;
+  align-items: left;
   flex-direction: column;
   height: 100%;
   width: 50%;
-  padding-left: 20px;
+  padding-left: 40px;
+  gap: 15px;
 `;
 const Flag = styled.div`
   height: 50%;
   width: 100%;
   background-color: blue;
 `;
+const Name = styled.h3`
+  margin: 0;
+`;
+const Info = styled.h4`
+margin: 0;
+`
+const InfoSpan = styled.span`
+color: grey;
+font-weight: 300;
+font-size: 14px;
+`
 
 const CardLower = () => {
   const { countries } = useCountry();
@@ -46,7 +58,17 @@ const CardLower = () => {
       <Left>
         <Flag></Flag>
       </Left>
-      <Right></Right>
+      <Right>
+        <Name>Germany</Name>
+        <Info>Population: <InfoSpan>5,000,000</InfoSpan> </Info>
+        <Info>Region: <InfoSpan>Europe</InfoSpan> </Info>
+        <Info>Capital: <InfoSpan>Berlin</InfoSpan> </Info>
+        <Info>Currency: <InfoSpan>Euro</InfoSpan> </Info>
+        <Info>Languages:<InfoSpan> German</InfoSpan> </Info>
+        <Info>Continent:<InfoSpan>South America</InfoSpan> </Info>
+        <Info>Area: <InfoSpan>2,000,000 km2</InfoSpan> </Info>
+        
+      </Right>
     </Container>
   );
 };
