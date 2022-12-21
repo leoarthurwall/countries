04fc63832bb6af93aabb5f90daa5caaa75f41021
leoarthurwall <leowall93@gmail.com>
2022-12-21 22:35:29 +0000

    added a seperate overlay for the country card as previously the container had the overlay and when the card was clicked anywhere it would close. To get the desired effect of clicking off the card or the cross, the seperate overlay was implemented

--- src/Components/CountryCard/CountryCard.tsx
+++ src/Components/CountryCard/CountryCard.tsx
@@ -17,7 +17,6 @@ const Container = styled.section<IcountryModalOpen>`
 
   width: 100vw;
   height: 100vh;
-  background-color: rgba(0, 0, 0, 0.7);
   z-index: 100;
   visibility: ${({ countryModalOpen }) => !countryModalOpen && "hidden"};
   opacity: ${({ countryModalOpen }) => (!countryModalOpen ? "0" : "1")};
@@ -31,8 +30,22 @@ const Card = styled.div`
   border-radius: 5px;
   display: flex;
   flex-direction: column;
+  z-index: 120;
 `;
 
+const Overlay = styled.div<IcountryModalOpen>`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 80;
+  visibility: ${({ countryModalOpen }) => !countryModalOpen && "hidden"};
+  opacity: ${({ countryModalOpen }) => (!countryModalOpen ? "0" : "1")};
+  transition: all 0.4s;
+`;
 interface IcountryModalOpen {
   countryModalOpen: boolean;
 }
@@ -40,14 +53,15 @@ interface IcountryModalOpen {
 const CountryCard: React.FC = (): ReactElement => {
   const { countryModalOpen, handleCountryModalClick } = useCountry();
   return (
-    <Container
-      countryModalOpen={countryModalOpen}
-      onClick={handleCountryModalClick}
-    >
+    <Container countryModalOpen={countryModalOpen}>
       <Card>
         <CardUpper />
         <CardLower />
       </Card>
+      <Overlay
+        onClick={handleCountryModalClick}
+        countryModalOpen={countryModalOpen}
+      />
     </Container>
   );
 };
