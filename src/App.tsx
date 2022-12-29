9a37a41da02b4552e583126eaafbd28f3efb6923
leoarthurwall <leowall93@gmail.com>
2022-12-29 23:49:41 +0000

    created a light theme and wrapped the app in a theme pgrovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Nav from "./Components/Nav/Nav";
 import Main from "./Components/Main/Main";
 import CountryCard from "./Components/CountryCard/CountryCard";
 
-
-const Container = styled.main`
+const Container = styled.body`
 height: 100%;
 width: 100vw;
-
+background-color: ${(props) => props.theme.body};
 display: flex:
 flex-direction: column;
 
 `;
+const lightTheme = {
+  body: "#FAFAFA",
+  fontColor: "#666",
+  fontSpanColor: "#808080",
+  cardColor: "#fff",
+};
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Main />
-      <CountryCard />
-    </Container>
+    <ThemeProvider theme={lightTheme}>
+      <Container>
+        <Nav />
+        <Main />
+        <CountryCard />
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CountryProvider } from "./Context/CountryContext";
 
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
