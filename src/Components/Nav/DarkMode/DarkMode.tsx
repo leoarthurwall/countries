58d399f2fe6a1a9f7234341ce2594cd8122b4784
leoarthurwall <leowall93@gmail.com>
2022-12-31 20:25:06 +0000

    created stationary toggle: now need to give it icons and functionality

--- src/Components/Nav/DarkMode/DarkMode.tsx
+++ src/Components/Nav/DarkMode/DarkMode.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-height: 20px;
-width: 40px;
-color: ${(props) => props.theme.fontColor};
-border: ${(props) => props.theme.fontColor} 1px solid;
+  height: auto;
+  width: 45px;
+  color: ${(props) => props.theme.fontColor};
+  border: ${(props) => props.theme.fontColor} 1px solid;
+  border-radius: 25px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+`;
 
-
-`
-
+const IconCircle = styled.div`
+  height: 20px;
+  width: 20px;
+  background-color: ${(props) => props.theme.fontColor};
+  border-radius: 50%;
+  margin: 1px;
+`;
 const DarkMode = () => {
   return (
-    <Container>DarkMode</Container>
-  )
-}
+    <Container>
+      <IconCircle></IconCircle>
+    </Container>
+  );
+};
 
-export default DarkMode
+export default DarkMode;
