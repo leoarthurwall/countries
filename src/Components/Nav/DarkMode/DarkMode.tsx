4ecc3c51a708ec31dd03e1aebac7bfc98d7fa3d8
leoarthurwall <leowall93@gmail.com>
2022-12-31 20:57:28 +0000

    the custom dark mode is now functional - just need to add icons inside

--- src/Components/Nav/DarkMode/DarkMode.tsx
+++ src/Components/Nav/DarkMode/DarkMode.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { useCountry } from "../../../Context/CountryContext";
 
-const Container = styled.div`
-  height: auto;
+const Container = styled.div<IisDarkMode>`
+  height: 22px;
   width: 45px;
   color: ${(props) => props.theme.fontColor};
   border: ${(props) => props.theme.fontColor} 1px solid;
   border-radius: 25px;
   display: flex;
-  justify-content: left;
   align-items: center;
+  cursor: pointer;
+  animation-duration: 0.5s;
 `;
 
-const IconCircle = styled.div`
+const IconCircle = styled.div<IisDarkMode>`
   height: 20px;
   width: 20px;
   background-color: ${(props) => props.theme.fontColor};
   border-radius: 50%;
   margin: 1px;
+  transform: ${({ isDarkMode }) => isDarkMode ? "translateX(0)" : "translateX(23px)" };
+  transition: all 0.15s ease-in;
 `;
+
+interface IisDarkMode {
+  isDarkMode: Boolean;
+}
+
 const DarkMode = () => {
+  const { isDarkMode, handleColorThemeClick } = useCountry();
   return (
-    <Container>
-      <IconCircle></IconCircle>
+    <Container onClick={handleColorThemeClick} isDarkMode={isDarkMode}>
+      <IconCircle isDarkMode={isDarkMode}></IconCircle>
     </Container>
   );
 };
