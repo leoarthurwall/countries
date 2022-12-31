cdfa5f337139c9cb863b658e80205f81d55869a5
leoarthurwall <leowall93@gmail.com>
2022-12-31 21:06:56 +0000

    added icons to the dark mode toggle. everything changes color theme when toggle is clicked

--- src/Components/Nav/DarkMode/DarkMode.tsx
+++ src/Components/Nav/DarkMode/DarkMode.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import styled from "styled-components";
 import { useCountry } from "../../../Context/CountryContext";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 const Container = styled.div<IisDarkMode>`
   height: 22px;
@@ -18,9 +18,15 @@ const IconCircle = styled.div<IisDarkMode>`
   height: 20px;
   width: 20px;
   background-color: ${(props) => props.theme.fontColor};
+  color: ${(props) => props.theme.mainBackgroundColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   border-radius: 50%;
   margin: 1px;
-  transform: ${({ isDarkMode }) => isDarkMode ? "translateX(0)" : "translateX(23px)" };
+  transform: ${({ isDarkMode }) =>
+    isDarkMode ? "translateX(0)" : "translateX(23px)"};
   transition: all 0.15s ease-in;
 `;
 
@@ -32,7 +38,9 @@ const DarkMode = () => {
   const { isDarkMode, handleColorThemeClick } = useCountry();
   return (
     <Container onClick={handleColorThemeClick} isDarkMode={isDarkMode}>
-      <IconCircle isDarkMode={isDarkMode}></IconCircle>
+      <IconCircle isDarkMode={isDarkMode}>
+        {isDarkMode ? <BsFillMoonFill size={14}/> : <BsFillSunFill size={14}/>}
+      </IconCircle>
     </Container>
   );
 };
